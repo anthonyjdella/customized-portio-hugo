8ef49c50fa5b889390c7f1406c276f97c0e99e0f
Anthony_Dellavecchia <leeznon@gmail.com>
2022-01-05 22:25:13 -0600

    feat(algolia): algolia search index works and json is created

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,59 +1,74 @@
-// const instantsearch = require('instantsearch.js');
-// import instantsearch from 'instantsearch.js';
-// console.log(instantsearch)
-
-
-
-// const instantsearch = require('instantsearch.js');
-
-// const search = instantsearch({
-//     appId: 'HPDLDN6WYS',
-//     apiKey: '6ca860e96696c35d0e40c3315f2cb1e4',
-//     indexName: 'anthonydellavecchia',
-//     routing: true
-// });
-
-// search.start();
-
-
-
-// import algoliasearch from '../node_modules/algoliasearch/lite';
-// import instantsearch from 'instantsearch.js';
-// import { searchBox, hits } from './instantsearch.js/es/widgets';
-
-
-// const searchClient = algoliasearch('HPDLDN6WYS', '6ca860e96696c35d0e40c3315f2cb1e4');
-
-// const search = instantsearch({
-//   indexName: 'anthonydellavecchia',
-//   searchClient,
-// });
-
-// search.addWidgets([
-//   searchBox({
-//     container: "#searchbox"
-//   }),
-
-//   hits({
-//     container: "#hits"
-//   })
-// ]);
-
-// search.start();
-
-
-// const instantsearch = require('instantsearch.js');
-
-// console.log("========")
-
-
 const search = instantsearch({
-    appId: 'HPDLDN6WYS',
-    apiKey: '6ca860e96696c35d0e40c3315f2cb1e4',
     indexName: 'anthonydellavecchia',
-    routing: true
+    routing: true,
+    searchClient: algoliasearch(
+        'HPDLDN6WYS',
+        '6ca860e96696c35d0e40c3315f2cb1e4'
+    )
 });
 
+// insights for analytics, but since client is null this doesnt work
+const insightsMiddleware = instantsearch.middlewares.createInsightsMiddleware({
+    insightsClient: null
+});
+
+ // initialize currentRefinements
+search.addWidget(
+    instantsearch.widgets.currentRefinements({
+        container: '#current-refined-values',
+        clearAll: false
+    })
+);
+
+// initialize clearRefinements
+search.addWidget(
+    instantsearch.widgets.clearRefinements({
+        container: '#clear-all',
+        templates: {
+        link: 'Reset everything'
+    },
+    autoHideContainer: false
+    })
+);
+
+// initialize pagination
+search.addWidget(
+    instantsearch.widgets.pagination({
+        container: '#pagination',
+        maxPages: 20,
+        // default is to scroll to 'body', here we disable this behavior
+        scrollTo: false
+    })
+);
+
+// initialize RefinementList
+search.addWidget(
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list',
+        attribute: 'tags'
+    })
+);
+
+// initialize SearchBox
+search.addWidget(
+    instantsearch.widgets.searchBox({
+        container: '#search-box',
+        placeholder: "Search Articles written by Anthony"
+    })
+);
+
+// initialize hits widget
+search.addWidget(
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            empty: 'No results',
+            item: '<article><p>Title: {{#helpers.highlight}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.highlight}}</p></article>'
+        }
+    })
+);
+
+search.use(insightsMiddleware);
 search.start();
 
 console.log(search);
